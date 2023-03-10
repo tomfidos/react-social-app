7874fc32aa46ae25e150200134985ca3332ece34
Tomasz Fidos <fidostomasz@gmail.com>
2023-03-10 17:59:43 +0100

    Added post with logout to App.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import AppNav from './components/AppNav';
 import MainRoutes from './routes/AppRoutes';
 
+const LOGOUT = 'https://akademia108.pl/api/social-app/user/logout';
+
 
 const App = () => {
 
     const [user, setUser] = useState({isLogged: false});
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + (user ? user.jwt_token : '');
 
     const setUserData = (user) => {
         setUser(user);
     }
 
     const logoutUser = () => {
-        setUser({isLogged: false});
+        axios
+            .post(LOGOUT)
+            .then(() => {
+                console.log(user.id + ' logged out.');
+                setUser({isLogged: false});
+            })
+            .catch(error => console.error(error));
     }
 
     return (
