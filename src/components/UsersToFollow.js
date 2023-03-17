82db527a0dd65127eee1017d9a27ff98959a185a
Tomasz Fidos <fidostomasz@gmail.com>
2023-03-17 19:35:36 +0100

    Corrected logic dealing with presenting recommendations and added follow user event handling.

--- src/components/UsersToFollow.js
+++ src/components/UsersToFollow.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import './UsersToFollow.css';
 
 const RECOMMENDATIONS = 'https://akademia108.pl/api/social-app/follows/recommendations';
+const FOLLOW = 'https://akademia108.pl/api/social-app/follows/follow';
 
 
 const UsersToFollow = () => {
@@ -17,27 +18,42 @@ const UsersToFollow = () => {
             .catch(error => console.error(error));
     }
 
+    const followUser = (event, leaderId) => {
+        event.preventDefault();
+
+        axios
+            .post(FOLLOW, { leader_id: leaderId })
+            .then()
+            .catch(error => console.error(error));
+    }
+
     useEffect(() => {
         getRecommendedUsers();
     }, []);
 
     return (
         <div className="form flex-row">
-            <div className="user-to-follow">
-                <img src={recommendedUsers.length > 0 ? recommendedUsers[0].avatar_url : null} alt="avatar"/>
-                <h4>{recommendedUsers.length > 0 ? recommendedUsers[0].username : null}</h4>
-                <button className="button no-margin">Follow</button>
-            </div>
-            <div className="user-to-follow">
-                <img src={recommendedUsers.length > 0 ? recommendedUsers[1].avatar_url : null} alt="avatar"/>
-                <h4>{recommendedUsers.length > 0 ? recommendedUsers[1].username : null}</h4>
-                <button className="button no-margin">Follow</button>
-            </div>
-            <div className="user-to-follow">
-                <img src={recommendedUsers.length > 0 ? recommendedUsers[2].avatar_url : null} alt="avatar"/>
-                <h4>{recommendedUsers.length > 0 ? recommendedUsers[2].username : null}</h4>
-                <button className="button no-margin">Follow</button>
-            </div>
+            {recommendedUsers.length >= 1 &&
+                <div className="user-to-follow">
+                    <img src={recommendedUsers[0].avatar_url} alt="avatar" />
+                    <h4>{recommendedUsers[0].username}</h4>
+                    <button className="button no-margin" onClick={event => followUser(event, recommendedUsers[0].id)}>Follow</button>
+                </div>
+            }
+            {recommendedUsers.length >= 2 &&
+                <div className="user-to-follow">
+                    <img src={recommendedUsers[1].avatar_url} alt="avatar" />
+                    <h4>{recommendedUsers[1].username}</h4>
+                    <button className="button no-margin" onClick={event => followUser(event, recommendedUsers[1].id)}>Follow</button>
+                </div>
+            }
+            {recommendedUsers.length >= 3 &&
+                <div className="user-to-follow">
+                    <img src={recommendedUsers[2].avatar_url} alt="avatar" />
+                    <h4>{recommendedUsers[2].username}</h4>
+                    <button className="button no-margin" onClick={event => followUser(event, recommendedUsers[2].id)}>Follow</button>
+                </div>
+            }
         </div>
     );
 }
