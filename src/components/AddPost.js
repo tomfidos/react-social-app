08caa884a3b40cf88bfb7d1c14fc63240d063413
Tomasz Fidos <fidostomasz@gmail.com>
2023-03-20 17:50:16 +0100

    Moved adding post to AddPost.

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -1,30 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 import './Post.css';
+
+const ADD_POST = 'https://akademia108.pl/api/social-app/post/add';
 
 
 const AddPost = (props) => {
 
     const [postContent, setPostContent] = useState('');
-    const [currentPostCounter, setCurrentPostCounter] = useState(0);
 
     const readNewPostContent = (event) => {
         setPostContent(event.target.value);
     }
 
-    const clearTextarea = (newPostCounter) => {
-        if (newPostCounter > currentPostCounter) {
-            setPostContent('');
-            setCurrentPostCounter(currentPostCounter + 1);
-        }
+    const addNewPost = (event) => {
+        event.preventDefault();
+
+        axios
+            .post(ADD_POST, {content: postContent})
+            .then(() => {
+                props.setPostError({
+                    isError: false,
+                    message: null,
+                });
+                setPostContent('');
+                props.getLatestPosts();
+            })
+            .catch(error => {
+                console.error(error);
+                props.setPostError({
+                    isError: true,
+                    message: error.message,
+                });
+            });
     }
 
-    useEffect(() => {
-        clearTextarea(props.postCounter);
-    }, [props.postCounter]);
-
     return (
-        <form className="form" onSubmit={(event) => props.onAddPost(event, postContent)}>
+        <form className="form" onSubmit={addNewPost}>
             <textarea className="input newPost" placeholder="Write a new post..." value={postContent} onChange={readNewPostContent} />
             <button type="submit" className="button addPostButton">add</button>
         </form>
