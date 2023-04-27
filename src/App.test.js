4a3c2e49a4ea79ce2ec83c505ff8680ca28040d3
Tomasz Fidos <fidostomasz@gmail.com>
2023-04-27 18:09:14 +0200

    Added tests for App.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App test', () => {
+
+  it('renders App elements', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    expect(screen.getByTestId('app')).toBeInTheDocument();
+    expect(screen.getByTestId('appNav')).toBeInTheDocument();
+  });
 });
